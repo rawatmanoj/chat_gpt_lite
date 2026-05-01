f55d59d69cc213a9a6ff28bbe00017e73a9c7b68
Manoj <manoj@Manojs-MacBook-Pro.local>
2026-05-01 11:49:37 +0530

    updated biagram

--- biagram.py
+++ biagram.py
@@ -1,3 +1,15 @@
+import torch 
 
 words = open('names.txt').read().splitlines()
-print(words[:10])
+biagram_count = {}
+min(len(name) for name in words)
+look_up_table = sorted(list(set(''.join(words))))
+
+print(look_up_table)
+for word in words:
+    ch = ['<S>'] + list(word) + ['<E>']
+    for char1,char2 in zip(ch,ch[1:]):
+        biagram_count[(char1,char2)] = biagram_count.get((char1,char2),0) + 1
+N = torch.zeros((28,28),dtype=torch.int32)
+print(N)
+# print(sorted(biagram_count.items(),key = lambda x:x[1],reverse=True))
