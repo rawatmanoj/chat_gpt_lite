82e6a1711d2c0197decf478cf0160be069dc49a6
Manoj <manoj@Manojs-MacBook-Pro.local>
2026-05-11 00:40:23 +0530

    updated biagram

--- biagram.py
+++ biagram.py
@@ -1,17 +1,42 @@
 import torch 
+import matplotlib.pyplot as plt
 
 words = open('names.txt').read().splitlines()
 biagram_count = {}
 min(len(name) for name in words)
 look_up_table = sorted(list(set(''.join(words))))
-stoi = {y:x for x,y in enumerate(look_up_table)}
-stoi['<S>'] = 26
-stoi['<E>'] = 27
-print(stoi)
+stoi = {y:x+1 for x,y in enumerate(look_up_table)}
+stoi['.'] = 0
+itos = {y:x for x,y in stoi.items()}
+N = torch.zeros((27,27),dtype=torch.int32)
 for word in words:
-    ch = ['<S>'] + list(word) + ['<E>']
+    ch = ['.'] + list(word) + ['.']
+    #print(ch)
     for char1,char2 in zip(ch,ch[1:]):
-        biagram_count[(char1,char2)] = biagram_count.get((char1,char2),0) + 1
-N = torch.zeros((28,28),dtype=torch.int32)
-# print(biagram_count)
-# print(sorted(biagram_count.items(),key = lambda x:x[1],reverse=True))
+        N[stoi[char1],stoi[char2]] += 1
+# plt.imshow(N)
+# plt.show()
+# print(N[0])
+p = N[0].float();
+p = p / p.sum();
+# print(p)
+g = torch.Generator().manual_seed(2147483647)
+# res = torch.multinomial(p,num_samples=1,replacement=True,generator=g).item()
+# print(res,itos[res],"resss")
+P = N.float()
+P = P / P.sum(1,keepdim=True)
+# print(P,"pppp")
+res = []
+ix = 0
+for i in range(50):
+ while True:
+    # p = N[ix].float();
+    # p = p / p.sum();
+    p = P[ix]
+    ix = torch.multinomial(p,1,True,generator=g).item()
+    res.append(itos[ix]);
+    # print(itos[ix])
+    if ix == 0:
+        break;
+    
+print(''.join(res))
