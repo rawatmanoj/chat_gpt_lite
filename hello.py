40351e75dd8a0b2ddc1e1e00f638e0aa22437ce4
Manoj <manoj@Manojs-MacBook-Pro.local>
2026-06-28 22:29:11 +0530

    print hi

---
+++ hello.py
@@ -0,0 +1,22 @@
+import math
+import numpy as np
+import asyncio as asyncio
+import requests
+import aiohttp
+from fastapi import FastAPI
+
+# def readFileLineByLine():
+#  with open('./test.txt') as f:
+#     for line in f.read().split('\n\n'):
+#        yield line
+
+# gen = readFileLineByLine()
+# print(next(gen),'--------');    
+# print(next(gen),'------xw');    
+# print(next(gen));    
+
+gen = (x for x in range(3))
+
+print('hi')
+
+
